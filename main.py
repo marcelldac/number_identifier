982000a29c9db6105140cd73b219e014ff447d9a
marcelldac <dacmarcell@gmail.com>
2023-10-03 17:52:17 -0300

    refactor: adiciona prompt para melhor usabilidade

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import phonenumbers
-from numero import number
 from phonenumbers import geocoder
 from phonenumbers import carrier
+
+print('Padrão de número: +5571992760849')
+number = input('Digite o número desejado no padrão determinado: ')
 
 ch_number = phonenumbers.parse(number, "CH") #localização
 ro_number = phonenumbers.parse(number, "RO") #operadora
 
-print(geocoder.description_for_number(ch_number, "pt-br"),"\n", carrier.name_for_number(ro_number, "pt-br"))
+print(geocoder.description_for_number(ch_number, "pt-br"))
+print(carrier.name_for_number(ro_number, "pt-br"))
--- numero.py
+++
@@ -1,2 +0,0 @@
-#deve colocar o numero com ddd. ex.:'+5511999999999'
-number = '+5571983558395'
